4bf03d2540914822c1ab7d1462f94c6f605b8c5b
flyq <flyq951@gmail.com>
2021-12-16 22:40:55 +0800

    add

--- src/heartbeat/lib.rs
+++ src/heartbeat/lib.rs
@@ -1,8 +1,8 @@
 use ic_cdk_macros::*;
-use ic_cdk::export::{candid};
+use ic_cdk::export::{candid, Principal};
 
-#[import(canister = "ControlCanister")]
-struct ControlCanister;
+// #[import(canister = "ControlCanister")]
+// struct ControlCanister;
 
 static mut COUNTER : Option<candid::Nat> = None;
 
@@ -14,20 +14,25 @@ fn init() {
 }
 
 #[query]
-fn get() -> candid::Nat{
+fn get() -> candid::Nat {
     unsafe {
         COUNTER.as_mut().unwrap().clone()
     }
 }
 
 #[update]
-async fn getNum() -> candid::Nat{
-    ControlCanister::get().await.0
+async fn getNum(p: Principal) -> candid::Nat {
+    let result: Result<(candid::Nat,), _> = ic_cdk::api::call::call(p, "get", ()).await;
+    result.unwrap().0
+    // ControlCanister::get().await.0
 }
 
 #[update]
-async fn getBalance() -> candid::Nat{
-    ControlCanister::getBalance().await.0
+async fn getBalance(p: Principal) -> candid::Nat{
+    let result: Result<(candid::Nat,), _> = ic_cdk::api::call::call(p, "getBalance", ()).await;
+    result.unwrap().0
+
+    // ControlCanister::getBalance().await.0
 }
 
 // #[export_name = "canister_heartbeat"]
